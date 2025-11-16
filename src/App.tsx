3b74df6cd6af067e61444f53451ef7736fa5f3ce
[dyad] <altaaf5mahomed@gmail.com>
2025-11-16 14:25:41 +0200

    Reverted all changes back to version 4fc532eeb0d4c657757f27ecc6b584d8e06a2089

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import ServicesPage from "./pages/ServicesPage";
+import ServicesPage from "./pages/ServicesPage"; // Import the new ServicesPage
 import NotFound from "./pages/NotFound";
-import React from "react";
+
+const queryClient = new QueryClient();
 
 const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Index />} />
-      <Route path="/services" element={<ServicesPage />} />
-      {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/services" element={<ServicesPage />} /> {/* New route for ServicesPage */}
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
 );
 
 export default App;
